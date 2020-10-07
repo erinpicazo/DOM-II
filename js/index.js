81ad052d0da2912e50b8674e0d6c7b33d8382cb4
Erin <59984668+erinpicazo@users.noreply.github.com>
2020-10-07 03:47:09 -0400

    git commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-// Your code goes here
+
+
+const nav = document.querySelector('.main-navigation');
+
+nav.addEventListener('mouseenter', e => console.log('I am watching you.'));
+nav.addEventListener('mouseleave', e => console.log('Wait.... come back!!!'));
+nav.addEventListener('wheel', e => (nav.style.color = 'pink'));
+nav.addEventListener('click', e => e.preventDefault());
+
+
+
+const home = document.querySelector('.home');
+home.addEventListener('selectstart', e => {
+    home.style.backgroundColor = 'blue';
+    home.style.color = 'purple';
+    alert("don't think so");
+  });
+
+  
+const header = document.querySelector('.intro');
+
+const imgs = document.querySelectorAll('img');
+imgs.forEach(img => {
+    img.addEventListener('copy', e => alert('Whatcah doing?'));
+    img.addEventListener('drag', e => alert('Are you stealing?'));
+  });
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn => {
+    btn.addEventListener('selectstart', e => {
+      e.stopPropagation();
+    });
+    btn.addEventListener('click', e => {
+      alert('Whoops! Technical Difficulties.');
+    });
+    btn.addEventListener('mouseover', e => (btn.style.backgroundColor = 'yellow'));
+  });
+  
+
+window.addEventListener('load', e => (header.innerHTML = ''));
+window.addEventListener('resize', e => (nav.style.color = 'orange'));
+
